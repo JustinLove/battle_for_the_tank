2ff4ec1decb6690128e94d4e6443322036ecca6c
Justin Love <git@JustinLove.name>
2015-09-16 08:19:58 -0500

    only display select for own slot

--- ui/mods/battle_for_the_tank/new_game.js
+++ ui/mods/battle_for_the_tank/new_game.js
@@ -6,8 +6,8 @@
   model.tankLightLaserMightBe = ko.observableArray(api.settings.definitions.ui.settings.tank_light_laser_is.options)
   model.tankLightLaserDescription = function(x) {return x}
 
-  $('.slot-player-loading').after(
-    '<div class="form-control-personality">'+
+  $('.slot-controls').prepend(
+    '<div class="form-control-personality" data-bind="if: slot.containsThisPlayer">'+
       '<select data-bind="options: $root.tankLightLaserMightBe, optionsText: $root.tankLightLaserDescription, selectPicker: $root.tankLightLaserIs" data-width="106px">' + 
       '</select>' +
     '</div>')
